5b53b61643263ce4c88ec385e7f7a18232568ef5
Your Name <you@example.com>
2023-05-16 10:06:15 +0000

    devops

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,13 @@
 FROM python:3.10
 
-RUN mkdir /home/app
+RUN mkdir -p /home/app
 
-ENV HOME=/home/app
-ENV APP_HOME=/home/app/web
+WORKDIR /home/app/
 
-RUN mkdir $APP_HOME
-RUN mkdir $APP_HOME/staticfiles
-RUN mkdir $APP_HOME/mediafiles
-WORKDIR $APP_HOME
+COPY . /home/app/
 
-ENV PYTHONDONTWRITEBYTECODE 1
-ENV PYTHONUNBUFFERED 1
-
-RUN pip install --upgrade pip
-
-COPY requirements.txt .
+COPY requirements.txt /home/app/
 
 RUN pip install -r requirements.txt
 
-COPY . /home/app/web
-
-CMD ["python", "manage.py runserver"]
+CMD ["python", "manage.py", "runserver"]
--- core/settings/base.py
+++ core/settings/base.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from decouple import config
 
@@ -93,11 +94,13 @@ LANGUAGE_CODE = "en-us"
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-    
-STATIC_URL = "static/"
-MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / "mediafiles"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = 'back_static/'
+STATIC_ROOT =  os.path.join(BASE_DIR, 'back_static')
+
+MEDIA_URL = "back_media/"
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'back_media')
+
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
